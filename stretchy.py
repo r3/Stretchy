0bef9d25e5cd126d4b9df9dfc064291635af9692
r3 <ryan.roler@gmail.com>
2012-12-06 22:42:09 -0800

    Cleaned up stretchy script and added CLI. Added documentation and refactored the ugly into (relative) pretty. Added latex rendered PNG of the equation used.

--- stretchy.py
+++ stretchy.py
@@ -1,40 +1,103 @@
+import argparse
 from math import pi, cos, sin, sqrt
 
 import numpy
 from matplotlib import pyplot
 
-# Given (TEMP)
-GRAV = 9.81  # [m/s^2]
-R_NAUGHT = 1  # [m]
-MASS = 5.9  # [g]
-SPRING = 0.072375  # [N/m]
-INCREMENT = pi / 6  # Rads
 
-# These two lists will keep the r and theta values for each point, therefore
-# the first point will be (r_vals[0], theta_vals[0])
-# Things are done this way because of the pyplot.polar signature
-r_vals = []
-theta_vals = []
-for theta in numpy.arange(0, 2 * pi, INCREMENT):
-    # The following 'quad_' prefixed vars are for use in the quadratic formula
-    quad_a = SPRING / 2 * cos(theta)
-    quad_b = (SPRING / 2 + MASS * GRAV / 2 * sin(theta) - R_NAUGHT
-              * MASS * SPRING / 2 * cos(theta) + MASS * GRAV * cos(theta))
-    quad_c = -SPRING * R_NAUGHT / 2 - 3 * MASS * GRAV * R_NAUGHT / 2
+def generate_points(increment, grav, r_naught, mass, spring):
+    """Provides a generator of points in polar coordinates
 
-    # Uses the previous vars in the quadratic formula (positive part)
-    # (-b + sqrt(b^2 - 4ac))/(2a)
-    stretch_length = (
-        -quad_b + sqrt(quad_b ** 2 - 4 * quad_a * quad_c)) / (2 * quad_a)
+    Based on the following equation (latex markup):
 
-    #r_vals.append(stretch_length + R_NAUGHT)
-    r_vals.append(stretch_length)
-    theta_vals.append(theta)
+        $r^2\left(\frac{k}{2}\Cos(\theta)\right)+
+         r\left(\frac{k+mg}{2}\sin(\theta)-
+            \frac{r_0mk}{2}\Cos(\theta)+mg\Cos(\theta)\right)+
+          \left(-\frac{kr_0-3mgr_0}{2}\right)$
+    """
+    for theta in numpy.arange(0, 2 * pi, increment):
+        quad_a = spring / 2 * cos(theta)
+        quad_b = (spring / 2 + mass * grav / 2 * sin(theta) - r_naught
+                  * mass * spring / 2 * cos(theta) + mass * grav * cos(theta))
+        quad_c = -spring * r_naught / 2 - 3 * mass * grav * r_naught / 2
 
-print("(r, theta)")
-for x, y in zip(r_vals, theta_vals):
-    print("({}, {})".format(x, y))
+        # Uses the previous vars in the quadratic formula (positive part)
+        # (-b + sqrt(b^2 - 4ac))/(2a)
+        stretch_length = (
+            -quad_b + sqrt(quad_b ** 2 - 4 * quad_a * quad_c)) / (2 * quad_a)
 
-# Plot the points
-pyplot.polar(r_vals, theta_vals, 'bo-')
-pyplot.show()
+        yield stretch_length + r_naught, theta
+
+
+def print_points(points):
+    """Display points numerically to standard output"""
+    print("(r, theta)")
+    for x, y in points:
+        print("({}, {})".format(x, y))
+
+
+def display_picture(points):
+    """Graphs the points given on a polar coordinate grid"""
+    r_vals = []
+    theta_vals = []
+    for r, theta in points:
+        r_vals.append(r)
+        theta_vals.append(theta)
+
+    pyplot.polar(r_vals, theta_vals, linestyle='solid', marker='o')
+    pyplot.show()
+
+
+def display_video(points):
+    """Produces an animated version of graph produced by 'display_picture'"""
+    raise NotImplementedError("This feature is not yet implemented")
+
+
+def main(args):
+    dispatch = {'picture': display_picture, 'video': display_video,
+                'data': print_points}
+    points = generate_points(args.increment,
+                             args.grav,
+                             args.r_naught,
+                             args.mass,
+                             args.spring)
+
+    for arg_name, func in dispatch.items():
+        if getattr(args, arg_name):
+            func(points)
+            break
+    else:
+        dispatch.get('data')(points)
+
+
+# Handle command line args
+parser = argparse.ArgumentParser(description="""Tool for modeling the length
+        of an elastic string with a mass attached to the end when swung in a
+        cirle perpendicular to the ground.""", epilog="""Default behavior is
+        to generate the data points and print them to standard output""")
+
+parser.add_argument('--video', '-v', help="Generate video file of graph",
+                    default=False, action='store_const',
+                    const=True)
+parser.add_argument('--picture', '-p', help="Generate picture file of graph",
+                    default=False, action='store_const',
+                    const=True)
+parser.add_argument('--data', '-d', help="Generate data points (polar)",
+                    default=False, action='store_const',
+                    const=True)
+
+parser.add_argument('--gravity', '-g', default=9.81, type=int, dest='grav',
+                    help="Acceleration due to gravity (9.81 [m/s^2]")
+parser.add_argument('--spring', '-s', default=0.072375, type=int,
+                    help="Spring constant of elastic line (0.072375 [N/m])")
+parser.add_argument('--length', '-r', default=1, type=int, dest='r_naught',
+                    help="Initial length of string (1 [m])")
+parser.add_argument('--mass', '-m', default=5.9, type=int,
+                    help="Grams of mass hanging from string (5.9 [g])")
+
+parser.add_argument('--increment', '-i', default=pi / 6, type=int,
+                    help="""Increment used between thetas in calculating
+                    length of string (pi/6).""")
+
+args = parser.parse_args()
+main(args)
